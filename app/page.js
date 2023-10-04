567e82ba3202b8e376be86bc51d378fb4c28f5cd
AltESV <ishigami.daniel@gmail.com>
2023-10-04 12:08:20 +0100

    suggest a feature

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import useWindowSize from 'react-use/lib/useWindowSize';
 import Confetti from 'react-confetti';
 import Modal from './Modal';
+import { PlusIcon } from '@heroicons/react/20/solid';
 
 
 export default function Home() {
@@ -90,14 +91,23 @@ function generateSessionId() {
     .catch(error => {
         console.error('Error voting:', error);
     });
-}
+  }
 
   return (
     <main className="px-4 py-8 bg-gray-900">
       {showConfetti && <Confetti width={width} height={height} />}
       {isModalOpen && <Modal onClose={() => setIsModalOpen(false)} details={modalDetails} />}
 
+      <div className='flex justify-between'>
       <h1 className="text-2xl font-bold mb-4 text-white">Roadmap</h1>
+      <a
+    href="mailto:team@myfana.com?subject=Suggestion for a Feature"
+    className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+    <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+    Suggest a Feature
+    </a>
+      </div>
+        
       <ul role="list" className="divide-y divide-gray-800">
         {features.map((feature) => (
           <li key={feature._id} className="flex justify-between gap-x-6 py-5">
@@ -120,6 +130,7 @@ function generateSessionId() {
           </li>
         ))}
       </ul>
+      
     </main>
   );
 }
